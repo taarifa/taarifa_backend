fb5db8ca15fb360bf282a59c1e58debe69239b49
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-04-14 13:54:33 +0100

    Add bare bones landing page with API doc

--- taarifa_backend/api.py
+++ taarifa_backend/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from taarifa_backend import app
 from models import BasicReport
 import models
@@ -7,6 +7,38 @@ import _help
 import pprint
 
 logger = app.logger
+
+def get_services():
+    response = {}
+
+    services = models.get_available_services()
+    for service_description in services:
+        logger.debug(service_description)
+        service_name = str(service_description.__name__)
+        logger.debug(service_name)
+
+        fields = service_description._fields
+        logger.debug(fields)
+
+        service = {}
+        field_dict = {}
+        for name, f in fields.iteritems():
+            if name in ['id', 'created_at']:
+                continue
+            field = {}
+            field['required'] = str(f.required)
+            field['type'] = _help.db_type_to_string(f.__class__)
+            field_dict[name] = field
+        service['fields'] = field_dict
+        for key in ['protocol_type', 'keywords', 'service_name', 'service_code', 'group', 'description']:
+            service[key] = getattr(service_description, key)
+        logger.debug(service)
+        response[service_name] = service
+    return response
+
+@app.route("/")
+def landing():
+    return render_template('landing.html', services=pprint.pformat(get_services()))
 
 @app.route("/reports", methods=['POST'])
 def receive_report():
@@ -44,32 +76,7 @@ def get_report(report_id = False):
 @app.route("/services", methods=['GET'])
 def get_list_of_all_services():
     # TODO: factor out the transformation from a service to json
-    response = {}
-
-    services = models.get_available_services()
-    for service_description in services:
-        logger.debug(service_description)
-        service_name = str(service_description.__name__)
-        logger.debug(service_name)
-
-        fields = service_description._fields
-        logger.debug(fields)
-
-        service = {}
-        field_dict = {}
-        for name, f in fields.iteritems():
-            if name in ['id', 'created_at']:
-                continue
-            field = {}
-            field['required'] = str(f.required)
-            field['type'] = _help.db_type_to_string(f.__class__)
-            field_dict[name] = field
-        service['fields'] = field_dict
-        for key in ['protocol_type', 'keywords', 'service_name', 'service_code', 'group', 'description']:
-            service[key] = getattr(service_description, key)
-        logger.debug(service)
-        response[service_name] = service
-    return jsonify(**response)
+    return jsonify(**get_services())
 
 def _save_report(report):
     report_ok = _verify_report(report)
