2502a6c54e4f87d3d344077cce5029e4bbca58d5
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-04-15 01:09:16 +0100

    Use MongoLab configuration from environment if available

--- taarifa_backend/__init__.py
+++ taarifa_backend/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 import logging
+from os import environ
+import urlparse
+
 # configure the logging
 logging.basicConfig(level='DEBUG', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {'DB': "taarifa_backend"}
-app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+if environ.get('MONGOLAB_URI'):
+    url = urlparse.urlparse(environ['MONGOLAB_URI'])
+    app.config['MONGODB_SETTINGS'] = {'username': url.username,
+                                      'password': url.password,
+                                      'host': url.hostname,
+                                      'port': url.port,
+                                      'db': url.path[1:]}
+else:
+    app.config['MONGODB_SETTINGS'] = {'db': "taarifa_backend"}
 
 db = MongoEngine(app)
 
