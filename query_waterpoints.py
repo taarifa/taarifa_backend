27cc37cdfb441b94c075974ba2c9bfb89aab3b84
Nico Weinert <nico.weinert@googlemail.com>
2013-06-01 13:14:11 +0100

    Added python file to query waterpoints

---
+++ query_waterpoints.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import requests
+import json
+
+def get_all_reports():
+    url = 'http://localhost:5000/reports'
+    params = dict(
+            service_code='wp001',
+            )
+
+    response = requests.get(url, params=params)
+    print response.url
+    print response.ok
+
+    data = json.loads(response.text)
+    reports = data['result']
+    print reports[0]
+    print len(reports)
+
+if __name__ == '__main__':
+    get_all_reports()
+
