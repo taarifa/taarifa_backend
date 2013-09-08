03d3b48db0feca69b924de206f3b0e80bcc75e9f
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-09-08 16:13:34 +0100

    Don't run tests against live site, use app.test_client instead

--- tests/test.py
+++ tests/test.py
@@ -1,53 +1,50 @@
+from base64 import b64encode
 import requests
 import json
 import unittest
 
-from taarifa_backend.models import clear_database, User
+from taarifa_backend import app, db
+from taarifa_backend.models import User
 from taarifa_backend.api import user_datastore
-
-BASE_URL = "http://localhost:5000"
 
 
 class TaarifaTest(unittest.TestCase):
 
     def setUp(self):
-        clear_database()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+        self.data = {"email": "taarifa_test@example.com",
+                     "password": "password"}
         user_datastore.create_user(email="username", password="password")
 
     def tearDown(self):
-        pass
+        db.connection.drop_database(app.config['MONGODB_SETTINGS']['db'])
 
     def _create_admin(self, data, expected_response_code, expected_increment,
                       auth=("username", "password")):
         user_count = User.objects().count()
         headers = {"content-type": "application/json"}
-        r = requests.post(BASE_URL + "/admin", data=json.dumps(data),
-                          headers=headers, auth=auth)
+        if auth:
+            headers["Authorization"] = "Basic " + b64encode(':'.join(auth))
+        r = self.app.post("/admin", data=json.dumps(data), headers=headers)
         self.assertEquals(expected_response_code, r.status_code)
         self.assertEquals(user_count + expected_increment,
                           User.objects().count())
         return r
 
     def test_create_admin(self):
-        data = {"email": "taafira_test@example.com",
-                "password": "password"}
-        self._create_admin(data, requests.codes.ok, 1)
+        self._create_admin(self.data, requests.codes.ok, 1)
 
     def test_create_admin_missing_fields(self):
-        data = {"email": "taafira_test@example.com",
-                "password": "password"}
+        data = [("password", "email", "taarifa_test@example.com"),
+                ("email", "password", "password")]
 
-        for expected_missing in data.keys():
-            data_copy = data.copy()
-            del data_copy[expected_missing]
-            r = self._create_admin(data_copy,
+        for missing, k, v in data:
+            r = self._create_admin({k: v},
                                    requests.codes.unprocessable_entity, 0)
-            response_data = r.json()
-            self.assertEquals([expected_missing], response_data["Missing"])
+            self.assertEquals([missing], eval(r.data)["Missing"])
 
     def test_create_admin_auth(self):
-        data = {"email": "taafira_test@example.com",
-                "password": "password"}
         for auth in [None, ("foo", "bar")]:
-            self._create_admin(data, requests.codes.unauthorized, 0,
+            self._create_admin(self.data, requests.codes.unauthorized, 0,
                                auth=auth)
