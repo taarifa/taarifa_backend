fca805226438ca4736088ca07fcb678e0efc5a9f
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-10-06 11:27:46 +0100

    Add document type describing Service and Field
    To allow creating dynamic schemas we need a document type describing
    a report schema. Each field is described the embedded document type
    Field capturing MongoEngine field arguments.

--- taarifa_backend/models.py
+++ taarifa_backend/models.py
@@ -4,6 +4,71 @@ from flask.ext.security import RoleMixin, UserMixin
 
 from taarifa_backend import db
 
+fieldmap = {
+    'BinaryField': db.BinaryField,
+    'BooleanField': db.BooleanField,
+    'ComplexDateTimeField': db.ComplexDateTimeField,
+    'DateTimeField': db.DateTimeField,
+    'DecimalField': db.DecimalField,
+    'DictField': db.DictField,
+    'DynamicField': db.DynamicField,
+    'EmailField': db.EmailField,
+    'EmbeddedDocumentField': db.EmbeddedDocumentField,
+    'FileField': db.FileField,
+    'FloatField': db.FloatField,
+    'GenericEmbeddedDocumentField': db.GenericEmbeddedDocumentField,
+    'GenericReferenceField': db.GenericReferenceField,
+    'GeoPointField': db.GeoPointField,
+    'ImageField': db.ImageField,
+    'IntField': db.IntField,
+    'ListField': db.ListField,
+    'MapField': db.MapField,
+    'ObjectIdField': db.ObjectIdField,
+    'ReferenceField': db.ReferenceField,
+    'SequenceField': db.SequenceField,
+    'SortedListField': db.SortedListField,
+    'StringField': db.StringField,
+    'URLField': db.URLField,
+    'UUIDField': db.UUIDField,
+}
+
+
+class Field(db.EmbeddedDocument):
+    """Field in a :class:`Service`."""
+    db_field = db.StringField(default=None)
+    required = db.BooleanField(default=False)
+    default = db.DynamicField(default=None)
+    unique = db.BooleanField(default=False)
+    unique_with = db.DynamicField(default=None)
+    primary_key = db.BooleanField(default=False)
+    choices = db.DynamicField(default=None)
+    help_text = db.StringField(default=None)
+    verbose_name = db.StringField(default=None)
+
+
+class Service(db.Document):
+    """A service schema served by the API."""
+    name = db.StringField(required=True)
+    fields = db.DictField(required=True)
+    description = db.StringField()
+    group = db.StringField()
+    keywords = db.ListField(db.StringField())
+    protocol_type = db.StringField()
+    service_name = db.StringField(required=True)
+    service_code = db.StringField(required=True, unique=True)
+
+
+def build_schema(service):
+    return type(str(service.name), (Reportable,),
+                dict(description=service.description,
+                     group=service.group,
+                     keywords=service.keywords,
+                     protocol_type=service.protocol_type,
+                     service_name=service.service_name,
+                     service_code=service.service_code,
+                     **service.fields)
+                )
+
 
 class Metadata(object):
 
