bccc9efaad8b79ac9c732f8e202adf8a1d8fc155
Dirk Gorissen <dgorissen@gmail.com>
2014-03-10 16:13:24 +0000

    Use font awesome markers, attach simple popup to each marker

--- taarifa_backend/static/base.js
+++ taarifa_backend/static/base.js
@@ -1,24 +1,38 @@
 function loadMap(){
-  $('#home').hide();
-  $('#mapcontainer').show();
-
   var map = L.map('map').setView([-6.3153, 35.15625], 6);
 
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+      attribution: '(c) OpenStreetMap contributors'
+  }).addTo(map);
 
   var markers = L.markerClusterGroup();
-
-  function addWaterPoint(waterpoint) {
-    markers.addLayer(L.marker([waterpoint.latitude,waterpoint.longitude]));
-  }
   map.addLayer(markers);
 
-  $.getJSON( "/reports", function( data ) {
-    var items = [];
+  function addWaterPoint(waterpoint) {
 
+    //TODO: set color depending on the waterpoint status
+    var color = 'blue';
+
+    var icon = L.AwesomeMarkers.icon({
+        prefix: 'glyphicon',
+        icon: 'tint',
+        markerColor: color
+    });
+
+    //TODO: use status
+    var popupTxt = ("<b>Waterpoint: " + waterpoint.id + "</b><br>Status: unknown.");
+
+    //TODO: attach the status to the marker so other code can use this (e.g., clustering)
+    var marker = L.marker([waterpoint.latitude,waterpoint.longitude], {icon: icon});
+    marker.bindPopup(popupTxt);
+    markers.addLayer(marker);
+  }
+
+
+  $.getJSON( "/reports", function( data ) {
+    //slice down to speed up testing
+    //data = data.slice(0,500);
     data.forEach( function (waterpoint, index) {
       addWaterPoint(waterpoint);
     });
