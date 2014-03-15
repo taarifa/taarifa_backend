21870eac1878e1e035a10c0e916234e8ce83fff9
Dirk Gorissen <dgorissen@gmail.com>
2014-03-15 00:19:55 +0000

    Update map ui: marker/cluster colors match waterpoint status

--- taarifa_backend/static/base.js
+++ taarifa_backend/static/base.js
@@ -1,3 +1,9 @@
+waterpointMarker = L.Marker.extend({
+   options: {
+      status: 'unknown'
+   }
+});
+
 function loadMap(){
   var map = L.map('map').setView([-6.3153, 35.15625], 6);
 
@@ -6,22 +12,33 @@ function loadMap(){
       attribution: '(c) OpenStreetMap contributors'
   }).addTo(map);
 
+  //TODO hardcoded map of status to classes
+  var classMap = {'functional': 'fixed-cluster',
+                  'not functional': 'broken-cluster',
+                  'in progress': 'inprog-cluster',
+                  'unknown': 'unknown-cluster'}
+
   //leaflet clustering plugin with control over what the clustering icon looks like
   var markers = new L.MarkerClusterGroup({
     iconCreateFunction: function(cluster) {
       var children = cluster.getAllChildMarkers();
-      var childCount = cluster.getChildCount();
 
-      //TODO loop over the children and determine the status of the cluster
+      //count the number of markers for each type
+      var counts = {};
+      children.forEach(function(m){
+        counts[m.options.status] = (counts[m.options.status] + 1) || 1;
+      });
+
+      //take the status with the highest count
+      var max_key = _.invert(counts)[_.max(counts)];
+      //get the matching class
+      var cls = classMap[max_key];
 
       //Using std divicon but could use something else
       return new L.DivIcon({
-        html: '<div><span>' + childCount + '</span></div>',
+        html: '<div><span>' + children.length + '</span></div>',
 
-             //TODO use all-broken-cluster, all-fixed-cluster, or mixed-cluster here depending on the status of the child waterpoints
-             //even better: single class with interpolation over the colour
-
-             className: 'leaflet-marker-icon marker-cluster marker-cluster-medium leaflet-zoom-animated leaflet-clickable',
+             className: 'leaflet-marker-icon marker-cluster marker-cluster-medium leaflet-zoom-animated leaflet-clickable ' + cls,
              iconSize: new L.Point(40, 40)
       });
     }
@@ -30,9 +47,21 @@ function loadMap(){
   map.addLayer(markers);
 
   function addWaterPoint(waterpoint) {
+    var status = waterpoint.status;
 
-    //TODO: set color depending on the waterpoint status
-    var color = 'blue';
+    //set color depending on the waterpoint status
+    //TODO use the defined classes instead of hardcoding colors?
+    var color = 'black';
+    if (status == 'functional'){
+        color = 'blue';
+    }else if(status == 'not functional'){
+        color = 'red';
+    }else if(status == 'in progress'){
+        color = 'green';
+    }else if(status == 'unknown'){
+        color = 'orange';
+    }else{
+    }
 
     var icon = L.AwesomeMarkers.icon({
         prefix: 'glyphicon',
@@ -40,11 +69,9 @@ function loadMap(){
         markerColor: color
     });
 
-    //TODO: use status
-    var popupTxt = ("<b>Waterpoint: " + waterpoint.id + "</b><br>Status: unknown.");
+    var popupTxt = ("<b>Waterpoint: " + waterpoint.id + "</b><br>Status: " + status);
 
-    //TODO: attach the status to the marker so other code can use this (e.g., clustering)
-    var marker = L.marker([waterpoint.latitude,waterpoint.longitude], {icon: icon});
+    var marker = new waterpointMarker([waterpoint.latitude,waterpoint.longitude], {icon: icon, status: status});
     marker.bindPopup(popupTxt);
     markers.addLayer(marker);
   }
