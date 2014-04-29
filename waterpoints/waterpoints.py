2f7f9df2915ad2a15a2bb6cf1db7d0ed270470c8
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-29 23:59:17 +0100

    Add waterpoint schema

--- waterpoints/waterpoints.py
+++ waterpoints/waterpoints.py
@@ -11,5 +11,127 @@ manager.add_command("runserver", Server(
     host='0.0.0.0')
 )
 
+waterpoint_schema = {
+    'date_recorded': {
+        'type': 'datetime',
+    },
+    'company': {
+        'type': 'string',
+    },
+    'region': {
+        'type': 'string',
+    },
+    'district': {
+        'type': 'string',
+    },
+    'lga_name': {
+        'type': 'string',
+    },
+    'ward': {
+        'type': 'string',
+    },
+    'village': {
+        'type': 'string',
+    },
+    'village_po': {
+        'type': 'string',
+    },
+    'village_re': {
+        'type': 'string',
+    },
+    'village_ph': {
+        'type': 'string',
+    },
+    'subvillage': {
+        'type': 'string',
+    },
+    'wpt_name': {
+        'type': 'string',
+    },
+    'wpt_code': {
+        'type': 'string',
+        # 'unique': True, FIXME: These are not unique in the dataset
+    },
+    'population': {
+        'type': 'integer',
+    },
+    'scheme_name': {
+        'type': 'string',
+    },
+    'water_perm': {
+        'type': 'string',
+    },
+    'catchment': {
+        'type': 'string',
+    },
+    'funder': {
+        'type': 'string',
+    },
+    'installer': {
+        'type': 'string',
+    },
+    'construction_year': {
+        'type': 'datetime',
+    },
+    'source_type': {
+        'type': 'string',
+    },
+    'extraction': {
+        'type': 'string',
+    },
+    'waterpoint': {
+        'type': 'string',
+    },
+    'status_detail': {
+        'type': 'string',
+    },
+    'status': {
+        'type': 'string',
+        'allowed': ['Functional', 'Not functional'],
+    },
+    'breakdown_year': {
+        'type': 'datetime',
+    },
+    'hardware_defect': {
+        'type': 'string',
+    },
+    'reason_wpt': {
+        'type': 'string',
+    },
+    'water_quantity': {
+        'type': 'string',
+    },
+    'water_quality': {
+        'type': 'string',
+    },
+    'scheme_management': {
+        'type': 'string',
+    },
+    'wp_management': {
+        'type': 'string',
+    },
+    'water_payment': {
+        'type': 'string',
+    },
+    'amount_tsh': {
+        'type': 'float',
+    },
+    'public_meeting': {
+        'type': 'string',
+    },
+    'comment': {
+        'type': 'string',
+    },
+    'gps_height': {
+        'type': 'float',
+    },
+    'latitude': {
+        'type': 'float',
+    },
+    'longitude': {
+        'type': 'float',
+    },
+}
+
 if __name__ == "__main__":
     manager.run()
